a3bbada4a0fc63e9ff1b6e314d6aed5a85732fde
Johan Fagerberg <johanringmann@gmail.com>
2024-04-02 10:20:18 +0000

    style: add prefix to logs from GitHub auth provider

--- src/server/auth/github-provider.ts
+++ src/server/auth/github-provider.ts
@@ -10,7 +10,7 @@ const CLIENT_ID = process.env["GITHUB_CLIENT_ID"];
 const CLIENT_SECRET = process.env["GITHUB_CLIENT_SECRET"];
 if (!CLIENT_ID || !CLIENT_SECRET) {
   console.warn(
-    "You do not have GitHub env variables GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET set up. It will be disabled as an auth provider."
+    "[auth.github] You do not have GitHub env variables GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET set up. It will be disabled as an auth provider."
   );
 }
 const JWT_SIGNING_KEY = process.env["JWT_SIGNING_KEY"]
@@ -18,7 +18,7 @@ const JWT_SIGNING_KEY = process.env["JWT_SIGNING_KEY"]
   : null;
 if (!JWT_SIGNING_KEY) {
   console.warn(
-    "You do not have a signing key set in env variable JWT_SIGNING_KEY. GitHub will be disabled as an auth provider."
+    "[auth.github] You do not have a signing key set in env variable JWT_SIGNING_KEY. GitHub will be disabled as an auth provider."
   );
 }
 
@@ -34,48 +34,57 @@ const getGitHubOauthUri = () => {
   );
   initiateUri.searchParams.set("scope", "user:email");
   return initiateUri;
-}
+};
 
 /** Verifies that the state matches when the user returns from the OAuth process */
 const verifyGitHubOauthState = async (context: APIContext) => {
   let state = "";
   const jwt = context.cookies.get(STATE_COOKIE_NAME)?.value;
   if (!jwt) {
-    console.warn("No state cookie present");
+    console.warn("[auth.github] No state cookie present");
     throw new Error("No state cookie present");
   }
   try {
     const { payload } = await jwtVerify(jwt, JWT_SIGNING_KEY!);
     state = `${payload["state"]}`;
   } catch (e) {
-    console.warn("State cookie failed to verify", e);
+    console.warn("[auth.github] State cookie failed to verify", e);
     throw new Error("State cookie failed to verify");
   }
   if (!state || state !== context.url.searchParams.get("state")) {
     console.warn(
-      `State failed to match (${state} !== ${context.url.searchParams.get(
+      `[auth.github] State failed to match (${state} !== ${context.url.searchParams.get(
         "state"
       )})`
     );
     throw new Error("State failed to match");
   }
-  return [true, ''];
-}
+  return [true, ""];
+};
 
 /** Completes the process after the user returns from the OAuth process, by exchanging the code for a token, and then using that token to get user information */
 const getEmailsFromGitHub = async (context: APIContext) => {
   if (context.url.searchParams.get("error")) {
-    console.warn("An error occurred when returning from GitHub:", {
-      error: context.url.searchParams.get("error"),
-      description: context.url.searchParams.get("error_description"),
-      uri: context.url.searchParams.get("error_uri"),
-    });
-    throw new Error(`The response from GitHub indicates that an error occurred ("${context.url.searchParams.get("error")}"). Please try again.`);
+    console.warn(
+      "[auth.github] An error occurred when returning from GitHub:",
+      {
+        error: context.url.searchParams.get("error"),
+        description: context.url.searchParams.get("error_description"),
+        uri: context.url.searchParams.get("error_uri"),
+      }
+    );
+    throw new Error(
+      `The response from GitHub indicates that an error occurred ("${context.url.searchParams.get(
+        "error"
+      )}"). Please try again.`
+    );
   }
   const code = context.url.searchParams.get("code");
   if (!code) {
-    console.warn("No code in search params");
-    throw new Error("The response from GitHub doesn't contain a code. Something went wrong in the authentication flow. Please try again.");
+    console.warn("[auth.github] No code in search params");
+    throw new Error(
+      "The response from GitHub doesn't contain a code. Something went wrong in the authentication flow. Please try again."
+    );
   }
   const ghResponse = await (
     await fetch(`https://github.com/login/oauth/access_token`, {
@@ -93,12 +102,12 @@ const getEmailsFromGitHub = async (context: APIContext) => {
   ).json();
   const ghAccessToken = ghResponse?.access_token;
   if (!ghAccessToken) {
-    console.warn("Invalid response from GitHub:", ghResponse);
+    console.warn("[auth.github] Invalid response from GitHub:", ghResponse);
     throw new Error(
       "Couldn't authenticate the given code with GitHub. Please try again."
     );
   }
-  console.log("Fetching emails with code", ghAccessToken);
+  console.log("[auth.github] Fetching emails with code", ghAccessToken);
   const emails = (await (
     await fetch("https://api.github.com/user/emails", {
       method: "GET",
@@ -110,7 +119,7 @@ const getEmailsFromGitHub = async (context: APIContext) => {
     })
   ).json()) as { email: string; primary: boolean }[] | undefined | null;
   return emails;
-}
+};
 
 /** The Provider exposes a uniform interface for authentication to happen through */
 const GitHubProvider: Provider = {
